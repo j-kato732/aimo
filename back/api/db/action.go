6219accbd75e8dfc7365f43648040542738b3a27
j-kato732 <jinini1212@gmail.com>
2021-10-19 21:45:04 +0900

    implemented user api

--- back/api/db/action.go
+++ back/api/db/action.go
@@ -699,3 +699,105 @@ func PutComprehensiveComment(ctx context.Context, request *pb.ComprehensiveComme
 
 	return nil
 }
+
+/*
+/user
+*/
+func GetUser(ctx context.Context, request *pb.UserModel) (*pb.UserModel, error) {
+	db, err := gorm.Open(sqlite.Open(db_path), &gorm.Config{})
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	con, err := db.DB()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer con.Close()
+
+	requestORM, err := request.ToORM(ctx)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	var responseORM *pb.UserModelORM
+
+	//get実行
+	query := "auth_id = ? AND period = ?"
+	if err = db.Where(query, requestORM.AuthId, requestORM.Period).Find(&responseORM).Error; err != nil {
+		return nil, err
+	}
+
+	// convert to PB from ORM
+	response, err := responseORM.ToPB(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+func PostUser(ctx context.Context, request *pb.UserModel) (int64, error) {
+	db, err := gorm.Open(sqlite.Open(db_path), &gorm.Config{})
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+	con, err := db.DB()
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+	defer con.Close()
+
+	// requestをORM型へ変換
+	requestORM, err := request.ToORM(ctx)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	// personalEvaテーブルが存在しない場合は作成する
+	isExist := db.Migrator().HasTable(requestORM.TableName())
+	if isExist != true {
+		db.AutoMigrate(requestORM)
+	}
+
+	// post実行
+	if err = db.Create(&requestORM).Error; err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	return requestORM.Id, nil
+}
+
+func PutUser(ctx context.Context, request *pb.UserModel) error {
+	db, err := gorm.Open(sqlite.Open(db_path), &gorm.Config{})
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	con, err := db.DB()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer con.Close()
+
+	requestORM, err := request.ToORM(ctx)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// put実行
+	if err = db.Model(&requestORM).Updates(requestORM).Error; err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
+	return nil
+}
--- back/api/grpc/server/main.go
+++ back/api/grpc/server/main.go
@@ -571,6 +571,64 @@ func (*getAimoService) PutComprehensiveComment(ctx context.Context, request *pb.
 	return response, nil
 }
 
+/*
+/user
+*/
+func (*getAimoService) GetUser(ctx context.Context, request *pb.UserModel) (*pb.GetUserResponse, error) {
+	var response *pb.GetUserResponse = new(pb.GetUserResponse)
+
+	// request null valid
+	// request format valid
+	// get実行
+	result, err := db.GetUser(ctx, request)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	// response組み立て
+	response.Response = newDefaultResponse(normal_code, "")
+	response.Result = new(pb.GetUserResponse_GetUserResult)
+	response.Result.User = result
+
+	return response, nil
+}
+
+func (*getAimoService) PostUser(ctx context.Context, request *pb.UserModel) (*pb.PostDefaultResponse, error) {
+	// request null validate
+	// request format valid
+	// post実行
+	result, err := db.PostUser(ctx, request)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	// response組み立て
+	var response *pb.PostDefaultResponse = new(pb.PostDefaultResponse)
+	response.Response = newDefaultResponse(normal_code, "")
+	response.Result = new(pb.PostDefaultResponse_PostResult)
+	response.Result.Id = result
+
+	return response, nil
+}
+
+func (*getAimoService) PutUser(ctx context.Context, request *pb.UserModel) (*pb.PutDefaultResponse, error) {
+	// request null valid
+	// request format valid
+	// put実行
+	err := db.PutUser(ctx, request)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	// response組み立て
+	var response *pb.PutDefaultResponse = new(pb.PutDefaultResponse)
+	response.Response = newDefaultResponse(normal_code, "")
+
+	return response, nil
+}
+
 func newDefaultResponse(status int64, message string) *pb.DefaultResponse {
 	default_response := new(pb.DefaultResponse)
 	default_response.Status = status
