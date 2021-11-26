c5e2bfec723b9b124414db090722d4848f366781
j-kato732 <jinini1212@gmail.com>
2021-11-26 22:08:34 +0900

    add kind of model validate personalEva evaluationBefore Evaluiation AchievementMean

--- back/api/proto/validate.go
+++ back/api/proto/validate.go
@@ -14,7 +14,48 @@ func (a AimModel) Validate() error {
 	return validation.ValidateStruct(&a,
 		validation.Field(&a.Period, validation.Required, validation.Length(6, 6)),
 		validation.Field(&a.UserId, validation.Required),
+		validation.Field(&a.AimItem, validation.Length(0, 500)),
+		validation.Field(&a.AchievementLevel, validation.Length(0, 500)),
+		validation.Field(&a.AchievementWeight, validation.Length(0, 3)),
+		validation.Field(&a.AchievementDifficultyBefore, validation.Length(0, 1)),
 		validation.Field(&a.AimNumber, validation.Required),
-		validation.Field(&a.AimItem, validation.Length(0, 5)),
+	)
+}
+
+func (a PersonalEvaModel) Validate() error {
+	return validation.ValidateStruct(&a,
+		validation.Field(&a.AimId, validation.Required),
+		validation.Field(&a.PersonalEvaluation, validation.Length(0, 500)),
+		validation.Field(&a.AchievementRate, validation.Length(0, 3)),
+		validation.Field(&a.AchievementDifficulty, validation.Length(0, 1)),
+	)
+}
+
+func (a EvaluationBeforeModel) Validate() error {
+	return validation.ValidateStruct(&a,
+		validation.Field(&a.AimId, validation.Required),
+		validation.Field(&a.EvaluatorNumber, validation.Required, validation.Length(0, 1)),
+		validation.Field(&a.Comment, validation.Length(0, 500)),
+	)
+}
+
+func (a EvaluationModel) Validate() error {
+	return validation.ValidateStruct(&a,
+		validation.Field(&a.AimId, validation.Required),
+		validation.Field(&a.EvaluatorNumber, validation.Required, validation.Length(0, 1)),
+		validation.Field(&a.Comment, validation.Length(0, 500)),
+		validation.Field(&a.AchievementRate, validation.Length(0, 100)),
+		validation.Field(&a.AchievementDifficulty, validation.Length(0, 1)),
+	)
+}
+
+func (a AchievementMeanModel) Validate() error {
+	return validation.ValidateStruct(&a,
+		validation.Field(&a.Period, validation.Required, validation.Length(6, 6)),
+		validation.Field(&a.UserId, validation.Required),
+		validation.Field(&a.AimNumber, validation.Required, validation.Length(1, 1)),
+		validation.Field(&a.AchievementMeanNumber, validation.Required, validation.Length(1, 1)),
+		validation.Field(&a.AchievementMean, validation.Length(0, 500)),
+		validation.Field(&a.FirstMonth, validation.Length(4, 5)),
 	)
 }
