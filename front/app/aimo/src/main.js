9bb21edc6dc779d7fe3acca707ff30f86125f52d
j-kato732 <jinini1212@gmail.com>
2022-01-06 22:27:08 +0900

    add auth0 login

--- front/app/aimo/src/main.js
+++ front/app/aimo/src/main.js
@@ -3,6 +3,24 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
+// Auth0で使用するコンフィグの取得
+import { domain, clientId } from '../auth_config.json';
+
+// Auth0プラグインの取得
+import { Auth0Plugin } from './auth';
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallBack: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
+
 Vue.config.productionTip = false
 
 new Vue({
--- front/app/aimo/src/views/MyPage.vue
+++ front/app/aimo/src/views/MyPage.vue
@@ -11,94 +11,115 @@
         <HeaderRightButton />
       </div>
     </div>
-    <br/>
+    <br />
     <Announce_board />
-    <br/>
+    <br />
     <div v-for="d in data" :key="d.id">
       <router-link to="/aimSettingSheet">
         <button>{{ d.financialYear_YY }} {{ d.mm }}</button>
       </router-link>
     </div>
-    <br/>
-    <br/>
-    <br/>
-    <router-link to="/setting_user">
+    <br />
+    <br />
+    <br />
+    <router-link v-if="$auth.isAuthenticated" to="/setting_user">
       <button>設定</button>
     </router-link>
-    <br/>
-    <br>
+    <br />
+    <br />
     <router-link to="/">
       <button>Hello World（ログアウト）</button>
     </router-link>
-    <br/>
+    <div class="home">
+      <div v-if="!$auth.loading">
+        <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+        <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+      </div>
+      <div>
+        <h1>以下に取得したユーザ情報を記載</h1>
+        <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
+      </div>
+    </div>
+    <br />
     <router-view />
   </div>
 </template>
 
 <script>
-  import Announce_board from '../components/Announce_board.vue'
-  import HeaderName from '../components/HeaderName.vue'
-  import HeaderLeftButton from '../components/HeaderLeftBotton.vue'
-  import HeaderRightButton from '../components/HeaderRightButton.vue'
-  import {getApi} from '@/api/MyPage' //axiosでAPI取得する処理をMyPage.jsに切り出し
+import Announce_board from "../components/Announce_board.vue";
+import HeaderName from "../components/HeaderName.vue";
+import HeaderLeftButton from "../components/HeaderLeftBotton.vue";
+import HeaderRightButton from "../components/HeaderRightButton.vue";
+import { getApi } from "@/api/MyPage"; //axiosでAPI取得する処理をMyPage.jsに切り出し
 
-  export default {
-    components: {
-      Announce_board,
-      HeaderName,
-      HeaderLeftButton,
-      HeaderRightButton
-    },
-    data(){
-      return {
-        data: []
+export default {
+  components: {
+    Announce_board,
+    HeaderName,
+    HeaderLeftButton,
+    HeaderRightButton,
+  },
+  data() {
+    return {
+      data: [],
+    };
+  },
+  created() {
+    // 下に書いたgetAPI関数をページ遷移時に呼び出す
+    (async () => {
+      await this.getAPI();
+    })();
+  },
+  methods: {
+    async getAPI() {
+      // MyPage.jsでaxiosから取得したAPIを使う
+      // asyncとawaitはセット
+      const subDatas = await getApi();
+      // 自分の使いたい形にデータを入れ直し（period（YYYYMM）をいろんな形に）
+      for (let data of subDatas.result.periods) {
+        let d = {};
+        d.id = data.id;
+        // data.periodをStringに変換して以下の切り出しを可能にした
+        const str = String(data.period);
+        d.financialYear = str.replace(/^(\d{4})/, "$1"); //2021
+        d.financialYear_YY = str.replace(/^\d{2}(\d{2})\d{2}/, "第$1期"); //21
+        d.period = str.replace(/^\d{4}(\d{2})/, "$1"); //05
+        d.mm = this.convertMMToHalfYear(d.period); //convertMMToHalfYear関数で上期or下期判別＆代入
+        this.data.push(d);
+        console.log(this.data);
       }
     },
-    created(){
-      // 下に書いたgetAPI関数をページ遷移時に呼び出す
-      (async()=>{
-        await this.getAPI();
-      })();
-    },
-    methods:{
-      async getAPI(){
-        // MyPage.jsでaxiosから取得したAPIを使う
-        // asyncとawaitはセット
-        const subDatas = await getApi();
-        // 自分の使いたい形にデータを入れ直し（period（YYYYMM）をいろんな形に）
-        for(let data of subDatas.result.periods){
-          let d = {};
-          d.id = data.id;
-          // data.periodをStringに変換して以下の切り出しを可能にした
-          const str = String(data.period);
-          d.financialYear = str.replace(/^(\d{4})/, '$1'); //2021
-          d.financialYear_YY = str.replace(/^\d{2}(\d{2})\d{2}/, '第$1期'); //21
-          d.period = str.replace(/^\d{4}(\d{2})/, '$1'); //05 
-          d.mm = this.convertMMToHalfYear(d.period); //convertMMToHalfYear関数で上期or下期判別＆代入
-          this.data.push(d);
-          console.log(this.data);
-        }
-      },
-      convertMMToHalfYear(period){
-        if(parseInt(period) === 5){
-          return "上期";
-        }else if(parseInt(period) === 11){
-          return "下期";
-        }
+    convertMMToHalfYear(period) {
+      if (parseInt(period) === 5) {
+        return "上期";
+      } else if (parseInt(period) === 11) {
+        return "下期";
       }
-    }
-  }
+    },
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin,
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .header {
-    display: flex;
-    justify-content: center;
-  }
-  .left { flex: 1; }
-  .title {
-    flex: 11;
-  }
-  .right { flex: 1; }
-
+.header {
+  display: flex;
+  justify-content: center;
+}
+.left {
+  flex: 1;
+}
+.title {
+  flex: 11;
+}
+.right {
+  flex: 1;
+}
 </style>
