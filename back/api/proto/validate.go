dd82d8aa13eb850fb5295f2e9d36c46750ee861e
j-kato732 <jinini1212@gmail.com>
2022-02-06 16:01:51 +0900

    validation int64エラー修正

--- back/api/proto/validate.go
+++ back/api/proto/validate.go
@@ -18,8 +18,6 @@ func (a AimModel) Validate() error {
 		validation.Field(&a.UserId, validation.Required),
 		validation.Field(&a.AimItem, validation.Length(0, 500)),
 		validation.Field(&a.AchievementLevel, validation.Length(0, 500)),
-		validation.Field(&a.AchievementWeight, validation.Length(0, 3)),
-		validation.Field(&a.AchievementDifficultyBefore, validation.Length(0, 1)),
 		validation.Field(&a.AimNumber, validation.Required),
 	)
 }
@@ -28,26 +26,25 @@ func (a PersonalEvaModel) Validate() error {
 	return validation.ValidateStruct(&a,
 		validation.Field(&a.AimId, validation.Required),
 		validation.Field(&a.PersonalEvaluation, validation.Length(0, 500)),
-		validation.Field(&a.AchievementRate, validation.Length(0, 3)),
-		validation.Field(&a.AchievementDifficulty, validation.Length(0, 1)),
 	)
 }
 
 func (a EvaluationBeforeModel) Validate() error {
 	return validation.ValidateStruct(&a,
 		validation.Field(&a.AimId, validation.Required),
-		validation.Field(&a.EvaluatorNumber, validation.Required, validation.Length(0, 1)),
+		validation.Field(&a.EvaluatorNumber, validation.Required),
 		validation.Field(&a.Comment, validation.Length(0, 500)),
+		validation.Field(&a.CommentUserId, validation.Required),
 	)
 }
 
 func (a EvaluationModel) Validate() error {
 	return validation.ValidateStruct(&a,
 		validation.Field(&a.AimId, validation.Required),
-		validation.Field(&a.EvaluatorNumber, validation.Required, validation.Length(0, 1)),
+		validation.Field(&a.EvaluatorNumber, validation.Required),
 		validation.Field(&a.Comment, validation.Length(0, 500)),
-		validation.Field(&a.AchievementRate, validation.Length(0, 100)),
-		validation.Field(&a.AchievementDifficulty, validation.Length(0, 1)),
+		validation.Field(&a.EvaluatorNumber, validation.Required),
+		validation.Field(&a.EvaluatorUserId, validation.Required),
 	)
 }
 
@@ -55,10 +52,9 @@ func (a AchievementMeanModel) Validate() error {
 	return validation.ValidateStruct(&a,
 		validation.Field(&a.Period, validation.Required, validation.Length(6, 6)),
 		validation.Field(&a.UserId, validation.Required),
-		validation.Field(&a.AimNumber, validation.Required, validation.Length(1, 1)),
-		validation.Field(&a.AchievementMeanNumber, validation.Required, validation.Length(1, 1)),
+		validation.Field(&a.AimNumber, validation.Required),
+		validation.Field(&a.AchievementMeanNumber, validation.Required),
 		validation.Field(&a.AchievementMean, validation.Length(0, 500)),
-		validation.Field(&a.FirstMonth, validation.Length(4, 5)),
 	)
 }
 
@@ -67,5 +63,6 @@ func (a ComprehensiveCommentModel) Validate() error {
 		validation.Field(&a.UserId, validation.Required),
 		validation.Field(&a.Period, validation.Required, validation.Length(6, 6)),
 		validation.Field(&a.ComprehensiveComment, validation.Length(0, 500)),
+		validation.Field(&a.CommentUserId, validation.Required),
 	)
 }
