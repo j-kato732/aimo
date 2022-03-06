379ae1f50e6e5db2a020e7e62eda1539272aab25
y-ito1231 <amnos.yuyu0627@gmail.com>
2022-03-06 17:26:07 +0900

    created postUser

---
+++ front/app/aimo/src/api/User.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+const baseUrl = "http://localhost"
+const port = 9002
+
+export async function postUser(
+  authId,
+  period,
+  lastName,
+  firstName,
+  departmentId,
+  jobId,
+  enrollmentflg,
+  adminFlg,
+  access_token
+){
+  const body = {
+    "authId": authId, 
+    "period": period,
+    "lastName": lastName,
+    "firstName": firstName,
+    "departmentId": departmentId,
+    "jobId": jobId,
+    "enrollmentflg": enrollmentflg,
+    "adminFlg": adminFlg
+  };
+  return new Promise((resolve,reject)=>{
+    axios
+      .post(`${baseUrl}:${port}/user`, 
+      body,
+      { headers: {
+        Authorization: `Bearer ${access_token}`,
+      },})
+      .then(res =>{
+        resolve(res.data);
+      })
+      .catch(err =>{
+        reject(err);
+    })
+  })
+}
