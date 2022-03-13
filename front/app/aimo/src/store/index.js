850df759883641246adf7dae7595950baae76ab0
y-ito1231 <amnos.yuyu0627@gmail.com>
2022-03-13 16:58:38 +0900

    added period at store

--- front/app/aimo/src/store/index.js
+++ front/app/aimo/src/store/index.js
@@ -5,14 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   getters:{
-    userId: state => state.userId
+    userId: state => state.userId,
+    period: state => state.period
   },
   state: {
-    userId: 0
+    userId: 0,
+    period: 0
   },
   mutations: {
     setUserId (state, userId) {
     state.userId = userId
-    }
+    },
+    setPeriod (state, period) {
+      state.period = period
+      }
   }
 })
